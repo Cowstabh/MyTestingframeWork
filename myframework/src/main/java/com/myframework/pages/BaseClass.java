b1f8b1a65026a139afa2034f5b11e5c05793f725
kaust <kaust@Kaustabh-Laptop>
2022-03-03 17:00:29 +0530

    comments change

--- myframework/src/main/java/com/myframework/pages/BaseClass.java
+++ myframework/src/main/java/com/myframework/pages/BaseClass.java
@@ -14,6 +14,7 @@ import com.myframework.utility.Helper;
 
 public class BaseClass {
 	
+	//this is base class
 	public WebDriver driver;
 	public ExcelDataProvider excel;
 	public ConfigDataProvider config;
